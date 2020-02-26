44329f57fb1dea179a06896913f95cf2b514ccba
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 09:49:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,13 +24,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
