aee0228d3b25d69b0a97da1527ec66495da08e0b
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-26 10:06:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ require 'pry'
 class Song
   
   extend Memorable::ClassMethods, Findable
-  include Memorable::InstanceMethods
+  include Memorable::InstanceMethods, Paramable
   
   attr_accessor :name
   attr_reader :artist
